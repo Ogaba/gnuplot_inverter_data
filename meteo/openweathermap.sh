80db4bbad8eb437ddc4bfc104dd23eff112c8c16
olivier <ogabathuler@free.fr>
2022-03-31 14:36:38 +0200

    Simplifications

--- meteo/openweathermap.sh
+++ meteo/openweathermap.sh
@@ -22,8 +22,11 @@ VERSION=1.0.0
 # Current day
 _DATE=`date +%Y%m%d`
 
+# Secret stored in a .gitignore file
+_TOKEN=`cat openweathermap.token`
+
 # Collect open meteo data from openweathermap.org every 10 minutes (see crontab)
 # Note : lat and lon are for my house
-curl "https://api.openweathermap.org/data/2.5/weather?lat=50.649923&lon=3.129666&timezone=Europe/Paris&appid=73a8086daf03642a83158b947745277d&units=metric&lang=fr" >> ~/developpement/meteo/data/${_DATE}_maison_villeneuve.json
+curl "https://api.openweathermap.org/data/2.5/weather?lat=50.649923&lon=3.129666&timezone=Europe/Paris&appid="${_TOKEN}"&units=metric&lang=fr" >> ~/developpement/meteo/data/${_DATE}_maison_villeneuve.json
 # Add UNIX return cariage
 echo "" >> ~/developpement/meteo/data/${_DATE}_maison_villeneuve.json
